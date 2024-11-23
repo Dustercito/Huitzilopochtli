4d0ec24b33cc884e07493350862098bcf4a34179
Itzel <itzel.chavez039@gmail.com>
2024-11-23 00:08:44 -0700

    agregue funcion para que el boton cambie al test

--- app/src/main/java/mx/uacj/recordatoriosapp/MainActivity.kt
+++ app/src/main/java/mx/uacj/recordatoriosapp/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Button
 import mx.uacj.recordatoriosapp.pantallas.PantallaCuestionario  // Asegúrate de importar correctamente la actividad
 
 class MainActivity : AppCompatActivity() {
+    lateinit var botonCambiarATest: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,6 +22,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        inicializarBotonATest()
 
         // Referencia al botón y configuración del clic
         val botonContinuar = findViewById<Button>(R.id.botonContinuarTest)
@@ -29,5 +31,8 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, PantallaCuestionario::class.java) // Aquí usas PantallaCuestionario::class.java
             startActivity(intent)
         }
+    }
+    fun inicializarBotonATest(){
+        botonCambiarATest = findViewById(R.id.botonContinuarTest)
     }
 }
